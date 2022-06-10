12ce030e44bf7b5ed3c29c96cc651b9afb08c279
Egor Kolesnikov <freethinkel@gmail.com>
2022-06-10 15:18:01 +0600

    update config

--- nvchad_custom/plugins/null-ls.lua
+++ nvchad_custom/plugins/null-ls.lua
@@ -30,7 +30,7 @@ M.setup = function()
       -- format on save
       on_attach = function(client)
          if client.resolved_capabilities.document_formatting then
-            vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
+            vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.format()"
          end
       end,
    }
