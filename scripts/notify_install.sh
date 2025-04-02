3f273932ce8d2b74ed3f5c714e38402f84d75fd3
Egor Kolesnikov <freethinkel@gmail.com>
2025-04-02 15:07:17 +0500

    fix

--- scripts/notify_install.sh
+++ scripts/notify_install.sh
@@ -3,5 +3,5 @@
 mkdir ~/.config/sounds/
 curl https://raw.githubusercontent.com/freethinkel/dotfiles/refs/heads/master/sounds/BitcoinAlert.mp3 --output ~/.config/sounds/
 
-curl https://raw.githubusercontent.com/freethinkel/dotfiles/refs/heads/master/bin/notify --output /usr/bin/
+curl https://raw.githubusercontent.com/freethinkel/dotfiles/refs/heads/master/scripts/notify --output /usr/bin/
 chmod +x /usr/bin/notify
