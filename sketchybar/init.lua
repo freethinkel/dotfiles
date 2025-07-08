43507de683f9af257ac2e54dddad91a11bd70b43
Egor Kolesnikov <freethinkel@gmail.com>
2025-07-08 21:06:01 +0500

    add tunnelblick icon

--- sketchybar/init.lua
+++ sketchybar/init.lua
@@ -27,3 +27,14 @@ end
 function trim(s)
 	return s:match("^%s*(.-)%s*$")
 end
+
+function split(inputstr, sep)
+	if sep == nil then
+		sep = "%s"
+	end
+	local t = {}
+	for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+		table.insert(t, str)
+	end
+	return t
+end
---
+++ sketchybar/items/tunnelblick.lua
@@ -0,0 +1,53 @@
+local vpn = "e.kolesnikov"
+
+local colors = require("colors")
+local is_connected = false
+
+local tunnelblick = sbar.add("item", {
+	icon = {
+		string = "􀆪",
+		color = colors.foreground,
+		font = {
+			size = 16.0,
+		},
+	},
+	position = "right",
+	update_freq = 10,
+})
+
+function check_connection()
+	local script =
+		'osascript -e \'tell application "Tunnelblick" to get state of first configuration where name = "vpn"\''
+	script = string.gsub(script, "vpn", vpn)
+
+	sbar.exec(script, function(result)
+		if result:match("CONNECTED") then
+			is_connected = true
+			tunnelblick:set({ icon = { string = "􀆪", color = colors.green } })
+		else
+			is_connected = false
+			tunnelblick:set({ icon = { string = "􀆪", color = colors.red } })
+		end
+	end)
+end
+
+tunnelblick:subscribe("mouse.clicked", function(env)
+	local action = is_connected and "disconnect" or "connect"
+	local script = 'osascript -e \'tell application "Tunnelblick" to __action__ "vpn"\''
+
+	script = string.gsub(script, "vpn", vpn)
+	script = string.gsub(script, "__action__", action)
+
+	sbar.exec(script, function(result)
+		is_connected = action == "connect" and true or false
+
+		tunnelblick:set({ icon = { string = "􀆪", color = is_connected and colors.red or colors.red } })
+	end)
+end)
+
+tunnelblick:subscribe("routine", function()
+	print("update_freq")
+	check_connection()
+end)
+
+check_connection()
